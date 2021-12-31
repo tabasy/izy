6f1aa9fbe57197af777ad0380bc605f0ef781aad
Mohsen Tabasi <m.tabasy@gmail.com>
2021-12-31 21:17:01 +0330

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ URL = 'https://github.com/tabasy/izy'
 EMAIL = 'm.tabasy@gmail.com'
 AUTHOR = 'Mohsen Tabasi'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.1'
+VERSION = '0.1.2'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
