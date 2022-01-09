1ac14934655cd34e19f6bcf811e088e9807f247b
Mohsen Tabasi <m.tabasy@gmail.com>
2022-01-09 12:39:51 +0330

    add ordered, update reorder functions

--- izy/sorting.py
+++ izy/sorting.py
@@ -1,4 +1,5 @@
-from typing import Union, Sequence, Mapping
+from typing import AbstractSet, MutableSequence, MutableSet, AbstractSet, Union, Sequence, Mapping
+from collections import OrderedDict
 import heapq
 
 
@@ -6,6 +7,7 @@ __all__ = ['ascending', 'descending', 'topk',
            'argtopk', 'argsort', 'argsorted',
            'argmin', 'argmax', 'reorder']
 
+Sortable = Union[Sequence, Mapping, AbstractSet]
 
 def ascending(x):
     return sorted(x, reverse=False)
@@ -13,39 +15,66 @@ def ascending(x):
 def descending(x):
     return sorted(x, reverse=True)
 
-def topk(x: Union[Sequence, Mapping], k, reverse=False, key=None):
+def topk(x: Sortable, k, reverse=False, key=None):
     if reverse or k < 0:
         return heapq.nsmallest(abs(k), x, key=key)
     else:
         return heapq.nlargest(k, x, key=key)
 
 
-def _getarg(x: Union[Sequence, Mapping]):
+def decide_arg(x: Sortable):
     if isinstance(x, Mapping):
         return x.keys()
     else:
         return range(len(x))
 
 
-def argtopk(x: Union[Sequence, Mapping], k, reverse=False):
-    return topk(_getarg(x), k, reverse=reverse, key=x.__getitem__)
+def argtopk(x: Sortable, k, reverse=False):
+    return topk(decide_arg(x), k, reverse=reverse, key=x.__getitem__)
 
-def argsort(x: Union[Sequence, Mapping], reverse=False):
-    return sorted(_getarg(x), key=x.__getitem__, reverse=reverse)
+def argsort(x: Sortable, reverse=False):
+    return sorted(decide_arg(x), key=x.__getitem__, reverse=reverse)
 
-def argsorted(x: Union[Sequence, Mapping], reverse=False):
+def argsorted(x: Sortable, reverse=False):
     return argsort(x, reverse)
 
 
-def argmax(x: Union[Sequence, Mapping]):
-    return max(_getarg(x), key=x.__getitem__)
+def argmax(x: Sortable):
+    return max(decide_arg(x), key=x.__getitem__)
 
-def argmin(x: Union[Sequence, Mapping]):
-    return min(_getarg(x), key=x.__getitem__)
+def argmin(x: Sortable):
+    return min(decide_arg(x), key=x.__getitem__)
 
-def reorder(x: Union[Sequence, Mapping], indices):   # reorder tuple? dict?!
-    return [x[i] for i in indices]
 
+def reorder(x: Sortable, indices):
+    if isinstance(x, (Mapping)):
+        return OrderedDict({key: x[key] for key in indices})        # dict() is ordered too, by OrderedDict() is more explicit
+
+    elif isinstance(x, (Sequence)):
+        reordered_x = (x[i] for i in indices)
+        if isinstance(x, (MutableSequence)):
+            return list(reordered_x)
+        else:
+            return tuple(reordered_x)
+    else:
+        raise NotImplementedError
+
+def ordered(x: Sortable, by_keys=False, reverse=False):
+    if isinstance(x, (Mapping)):
+        if by_keys:
+            sorted_keys = sorted(x.keys(), reverse=reverse)
+        else:
+            sorted_keys = argsorted(x, reverse=reverse)
+        return reorder(x, sorted_keys)
+
+    elif isinstance(x, (Sequence, AbstractSet)):
+        sorted_x = sorted(x, reverse=reverse)
+        if isinstance(x, (MutableSequence, MutableSet)):
+            return sorted_x
+        else:
+            return tuple(sorted_x)
+    else:
+        raise NotImplementedError
 
 if __name__ == '__main__':
 
@@ -69,14 +98,15 @@ if __name__ == '__main__':
 
     # print(f'{reorder(mylist, argsort(mylist)) = }\n')
 
-    # mydict = {'a': 1, 'b': 4, 'c': -1}
+    mydict = {'a': 1, 'b': 4, 'c': -1}
 
-    # print('\nfor dicts, keys are considered as indices:')
-    # print(f'{mydict = }\n')
+    print('\nfor dicts, keys are considered as indices:')
+    print(f'{mydict = }\n')
     
-    # print(f'{argmin(mydict) = }\n')
-    # print(f'{argmax(mydict) = }\n')
-    # print(f'{argsort(mydict) = }\n')
-    # print(f'{reorder(mydict, argsort(mydict)) = }')
-
-
+    print(f'{argmin(mydict) = }\n')
+    print(f'{argmax(mydict) = }\n')
+    print(f'{argsort(mydict) = }\n')
+    print(f'{reorder(mydict, argsort(mydict)) = }')
+    print(f'{reorder(mydict, argsort(mydict)) = }')
+    print(f'{ordered(mydict) = }')
+    print(f'{ordered(mydict, by_keys=True) = }')
